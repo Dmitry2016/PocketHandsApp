3ad05d40b04717c0e42c5ec0183278ae989fead7
Dmitry Blazhko <dmitriyblazhko@mac.com>
2024-01-31 01:12:48 +0300

    Created base project

---
+++ PocketHands/PocketHands/PocketHandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketHandsApp.swift
+//  PocketHands
+//
+//  Created by Blazhko Dmitry on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct PocketHandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
